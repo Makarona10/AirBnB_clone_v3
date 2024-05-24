bc62420a625d9654ef20d2488c69edef75b5065d
Makarona10 <ahmedneyssi@gmail.com>
2024-05-25 00:36:30 +0300

    API initiated

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """The app module"""
 
-from flask import Flask, make_response
+from flask import Flask
 from api.v1.views import app_views
 from models import storage
 from os import getenv
@@ -11,11 +11,11 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def teardown_appcontext():
+def tear_it_down():
     """Teardown flask app"""
     storage.close()
 
 if __name__ == "__main__":
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = getenv('HBNB_API_PORT', '5000')
+    PORT = int(getenv('HBNB_API_PORT', '5000'))
     app.run(host=HOST, port=PORT, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,8 @@
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status')
 def api_status():
+    """Return the status of api"""
     return jsonify(status="OK")
