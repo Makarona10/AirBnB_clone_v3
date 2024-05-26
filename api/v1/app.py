a07dc6d115cefb79b62aa80af6b4b44e13eb9d4d
Makarona10 <ahmedneyssi@gmail.com>
2024-05-26 05:58:48 +0300

    API fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,6 +7,8 @@ from models import storage
 from os import getenv
 
 
+HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+PORT = int(getenv('HBNB_API_PORT', '5000'))
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
@@ -18,6 +20,4 @@ def tear_it_down(e):
 
 
 if __name__ == "__main__":
-    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = int(getenv('HBNB_API_PORT', '5000'))
     app.run(host=HOST, port=PORT, threaded=True)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """The Blueprint for all the project"""
 
+from flask import Blueprint
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -9,7 +10,6 @@ from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
-from flask import Blueprint
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
