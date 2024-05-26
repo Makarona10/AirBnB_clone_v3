e5377bfda7ec564ec6bc85db44a864334f0bc9d9
Makarona10 <ahmedneyssi@gmail.com>
2024-05-26 06:05:15 +0300

    API fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """The app module"""
 
+import os
 from flask import Flask
 from api.v1.views import app_views
 from models import storage
-from os import getenv
 
 
-HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-PORT = int(getenv('HBNB_API_PORT', '5000'))
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
+
 
 @app.teardown_appcontext
 def tear_it_down(e):
@@ -20,4 +19,6 @@ def tear_it_down(e):
 
 
 if __name__ == "__main__":
+    HOST = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(os.getenv('HBNB_API_PORT', '5000'))
     app.run(host=HOST, port=PORT, threaded=True)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,7 @@
 """The Blueprint for all the project"""
 
 from flask import Blueprint
-# from api.v1.views.index import *
+from api.v1.views.index import *
 # from api.v1.views.states import *
 # from api.v1.views.cities import *
 # from api.v1.views.amenities import *
@@ -12,4 +12,4 @@ from flask import Blueprint
 # from api.v1.views.places_amenities import *
 
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+app_views = Blueprint(url_prefix='/api/v1')
