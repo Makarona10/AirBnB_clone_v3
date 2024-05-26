0bfddb93f11501b20b0992f5f0699925356c33ed
Makarona10 <ahmedneyssi@gmail.com>
2024-05-26 06:44:14 +0300

    API initiated

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-"""The Blueprint for all the project"""
-
+'''Contains the blueprint for the API.'''
 from flask import Blueprint
+
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+'''The blueprint for the AirBnB clone API.'''
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -10,6 +14,3 @@ from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-"""The blueprint for the project"""
