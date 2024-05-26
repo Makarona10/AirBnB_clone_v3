b87eb15d0e3546f7661e3a71cc168d6f6d31d742
Makarona10 <ahmedneyssi@gmail.com>
2024-05-26 06:35:28 +0300

    API initiated

--- api/v1/app.py
+++ api/v1/app.py
@@ -20,5 +20,5 @@ def tear_it_down(e):
 
 if __name__ == "__main__":
     HOST = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = int(os.getenv('HBNB_API_PORT', '5000'))
+    PORT = os.getenv('HBNB_API_PORT', '5000')
     app.run(host=HOST, port=PORT, threaded=True)
