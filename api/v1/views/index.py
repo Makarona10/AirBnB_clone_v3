5c6f2a52715bf522c3b65c38ee6b1b618f9ffe1b
Makarona10 <ahmedneyssi@gmail.com>
2024-05-26 06:31:15 +0300

    Task 4 done

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,30 @@
 
 from api.v1.views import app_views
 from flask import jsonify
-
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 @app_views.route('/status')
 def get_status():
     """Return the status of api"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def get_count():
+    """Returns number of objects"""
+    objects = {
+        'amenities': Amenity,
+        'cities': City,
+        'places': Place,
+        'reviews': Review,
+        'states': State,
+        'users': User
+    }
+    for k, val in objects.items():
+        objects[k] = storage.count(val)
+    return jsonify(objects)
