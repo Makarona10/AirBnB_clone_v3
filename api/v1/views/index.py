4beb54e72c6ca0a4e31f24dd4da70aa8744068ea
Makarona10 <ahmedneyssi@gmail.com>
2024-05-26 06:46:59 +0300

    API initiated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """The index file for views of the project"""
 
+
 from api.v1.views import app_views
 from flask import jsonify, make_response
 from models import storage
@@ -11,10 +12,12 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
+
 @app_views.route('/status')
 def get_status():
     """Return the status of api"""
     return make_response(jsonify({"status": "OK"}))
+
 
 @app_views.route('/stats')
 def get_count():
