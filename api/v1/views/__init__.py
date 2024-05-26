abd6027d2a91c65807b284e32fd79ffa6eeb0894
Makarona10 <ahmedneyssi@gmail.com>
2024-05-26 06:41:51 +0300

    API initiated

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,6 +2,10 @@
 """The Blueprint for all the project"""
 
 from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+"""The blueprint for the project"""
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -10,7 +14,3 @@ from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
-
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-"""The blueprint for the project"""
