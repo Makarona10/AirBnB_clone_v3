2757f16eef1bd7941248ecf84bbf302f5de46bc0
Makarona10 <ahmedneyssi@gmail.com>
2024-05-28 02:48:23 +0300

    users work done

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -27,12 +27,12 @@ def get_user(user_id):
 @app_views.route('/users/<user_id>', methods=['DELETE'])
 def delete_user(user_id):
     """Deletes a user"""
-    all_users = storage.all("User").values()
-    user_obj = [obj.to_dict() for obj in all_users if obj.id == user_id]
-    if user_obj == []:
+    users = storage.all("User").values()
+    user = [obj.to_dict() for obj in users if obj.id == user_id]
+    if not user:
         abort(404)
-    user_obj.remove(user_obj[0])
-    for obj in all_users:
+    user.remove(user[0])
+    for obj in users:
         if obj.id == user_id:
             storage.delete(obj)
             storage.save()
@@ -42,11 +42,12 @@ def delete_user(user_id):
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
 def create_user():
     """Creates a new user"""
-    if not request.get_json():
+    body = request.get_json()
+    if not body:
         abort(400, "Not a JSON")
-    if "email" not in request.get_json():
+    if "email" not in body:
         abort(400, "Missing name")
-    if "password" not in request.get_json():
+    if "password" not in body:
         abort(400, "Missing name")
     new_user = User(email=request.json['email'],
                     password=request.json['password'])
@@ -58,21 +59,21 @@ def create_user():
 @app_views.route('/users/<user_id>', methods=['PUT'])
 def updates_user(user_id):
     """Updates a User object"""
-    all_users = storage.all("User").values()
-    user_obj = [obj.to_dict() for obj in all_users if obj.id == user_id]
-    if user_obj == []:
+    users = storage.all("User").values()
+    user = [obj.to_dict() for obj in users if obj.id == user_id]
+    if not user:
         abort(404)
     if not request.get_json():
         abort(400, 'Not a JSON')
     try:
-        user_obj[0]['first_name'] = request.json['first_name']
+        user[0]['first_name'] = request.json['first_name']
     except KeyError:
         pass
     try:
-        user_obj[0]['last_name'] = request.json['last_name']
+        user[0]['last_name'] = request.json['last_name']
     except KeyError:
         pass
-    for obj in all_users:
+    for obj in users:
         if obj.id == user_id:
             try:
                 if request.json['first_name'] is not None:
@@ -85,4 +86,4 @@ def updates_user(user_id):
             except KeyError:
                 pass
     storage.save()
-    return make_response(jsonify(user_obj[0]), 200)
+    return make_response(jsonify(user[0]), 200)
