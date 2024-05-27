f541ff8cc415ae735c060632af91fe5a234a910c
Makarona10 <ahmedneyssi@gmail.com>
2024-05-28 02:40:18 +0300

    users work done

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """The users module"""
 
-from flask import jsonify, abort, request, make_response
 from api.v1.views import app_views
+from flask import jsonify, abort, request, make_response
 from models import storage
 from models.user import User
 
@@ -17,9 +17,9 @@ def get_users():
 @app_views.route('/users/<user_id>', methods=['GET'])
 def get_user(user_id):
     """Retrieves a user"""
-    users = storage.all("User").values()
-    user_obj = [obj.to_dict() for obj in users if obj.id == user_id]
-    if not user_obj:
+    all_users = storage.all("User").values()
+    user_obj = [obj.to_dict() for obj in all_users if obj.id == user_id]
+    if user_obj == []:
         abort(404)
     return jsonify(user_obj[0])
 
@@ -27,26 +27,26 @@ def get_user(user_id):
 @app_views.route('/users/<user_id>', methods=['DELETE'])
 def delete_user(user_id):
     """Deletes a user"""
-    users = storage.all("User").values()
-    user = [obj.to_dict() for obj in users if obj.id == user_id]
-    if not user:
+    all_users = storage.all("User").values()
+    user_obj = [obj.to_dict() for obj in all_users if obj.id == user_id]
+    if user_obj == []:
         abort(404)
-    user.remove(user[0])
-    for obj in users:
+    user_obj.remove(user_obj[0])
+    for obj in all_users:
         if obj.id == user_id:
             storage.delete(obj)
             storage.save()
-    return make_response(jsonify({}), 200)
+    return jsonify({}), 200
 
 
-@app_views.route('/users', methods=['POST'])
+@app_views.route('/users/', methods=['POST'])
 def create_user():
     """Creates a new user"""
     if not request.get_json():
         abort(400, "Not a JSON")
     if "email" not in request.get_json():
         abort(400, "Missing name")
-    if "password" not in request.get_json():
+    if 'password' not in request.get_json():
         abort(400, "Missing name")
     new_user = User(email=request.json['email'],
                     password=request.json['password'])
@@ -58,21 +58,21 @@ def create_user():
 @app_views.route('/users/<user_id>', methods=['PUT'])
 def updates_user(user_id):
     """Updates a User object"""
-    users = storage.all("User").values()
-    user = [obj.to_dict() for obj in users if obj.id == user_id]
-    if not user:
+    all_users = storage.all("User").values()
+    user_obj = [obj.to_dict() for obj in all_users if obj.id == user_id]
+    if user_obj == []:
         abort(404)
     if not request.get_json():
         abort(400, 'Not a JSON')
     try:
-        user[0]['first_name'] = request.json['first_name']
+        user_obj[0]['first_name'] = request.json['first_name']
     except:
         pass
     try:
-        user[0]['last_name'] = request.json['last_name']
+        user_obj[0]['last_name'] = request.json['last_name']
     except:
         pass
-    for obj in users:
+    for obj in all_users:
         if obj.id == user_id:
             try:
                 if request.json['first_name'] is not None:
@@ -85,4 +85,4 @@ def updates_user(user_id):
             except:
                 pass
     storage.save()
-    return make_response(jsonify(user[0]), 200)
+    return make_response(jsonify(user_obj[0]), 200)
