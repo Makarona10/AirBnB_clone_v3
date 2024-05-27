4818fdb38bcfe20c909182fdf2e71017c250acb0
Makarona10 <ahmedneyssi@gmail.com>
2024-05-28 02:43:00 +0300

    users work done

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -39,7 +39,7 @@ def delete_user(user_id):
     return jsonify({}), 200
 
 
-@app_views.route('/users/', methods=['POST'], strict_slashes=False)
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
 def create_user():
     """Creates a new user"""
     if not request.get_json():
