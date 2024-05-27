51a12661d6c71fdd776556fea8cf3ca2bccec67b
Makarona10 <ahmedneyssi@gmail.com>
2024-05-27 16:32:17 +0300

    states work done

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,6 +6,7 @@ from models import storage
 from models.state import State
 from api.v1.views import app_views
 from flask import make_response, jsonify, abort, request
+from werkzeug.exceptions import BadRequest
  
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
@@ -31,9 +32,9 @@ def delete_state(state_id):
 def add_state():
     body = request.get_json(force=True)
     if not body:
-        return make_response(jsonify({"error": "Not a JSON"}), 400)
+        raise BadRequest(description='Not a JSON')
     if "name" not in body:
-        return make_response(jsonify({"error": "Missing name"}), 400)
+        raise BadRequest(description='Missing name')
     state = State(**body)
     state.save()
     return make_response(jsonify(state.to_dict()), 201)
