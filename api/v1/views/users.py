8cf9d66de902366307ea888ca5662b29df2b1512
Makarona10 <ahmedneyssi@gmail.com>
2024-05-28 02:44:15 +0300

    users work done

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -58,21 +58,15 @@ def create_user():
 @app_views.route('/users/<user_id>', methods=['PUT'])
 def updates_user(user_id):
     """Updates a User object"""
-    all_users = storage.all("User").values()
-    user_obj = [obj.to_dict() for obj in all_users if obj.id == user_id]
+    users = storage.all("User").values()
+    user_obj = [obj.to_dict() for obj in users if obj.id == user_id]
     if user_obj == []:
         abort(404)
     if not request.get_json():
         abort(400, 'Not a JSON')
-    try:
-        user_obj[0]['first_name'] = request.json['first_name']
-    except:
-        pass
-    try:
-        user_obj[0]['last_name'] = request.json['last_name']
-    except:
-        pass
-    for obj in all_users:
+    user_obj[0]['first_name'] = request.json['first_name']
+    user_obj[0]['last_name'] = request.json['last_name']
+    for obj in users:
         if obj.id == user_id:
             try:
                 if request.json['first_name'] is not None:
