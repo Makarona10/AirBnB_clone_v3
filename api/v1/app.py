b1bba63b211c9774e6e71eade781ff2ef3c614f9
Makarona10 <ahmedneyssi@gmail.com>
2024-05-28 03:29:04 +0300

    CORS added

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,12 +5,14 @@ import os
 from flask import Flask, jsonify, make_response
 from api.v1.views import app_views
 from models import storage
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+HOST = os.getenv('HBNB_API_HOST', '0.0.0.0')
+PORT = os.getenv('HBNB_API_PORT', '5000')
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
-
+CORS(app, resources={'/*': {'origins': HOST}})
 
 @app.teardown_appcontext
 def tear_it_down(e):
@@ -24,6 +26,4 @@ def handler_for_404(err):
 
 
 if __name__ == "__main__":
-    HOST = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = os.getenv('HBNB_API_PORT', '5000')
     app.run(host=HOST, port=PORT, threaded=True)
