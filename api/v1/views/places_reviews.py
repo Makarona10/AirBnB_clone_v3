d6548a3f1aefb3e10eb7cc57eccb279befac7620
Makarona10 <ahmedneyssi@gmail.com>
2024-05-28 03:19:27 +0300

    places work done

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,55 +1,55 @@
 #!/usr/bin/python3
-"""API endpoint"""
-from flask import abort, jsonify, request
+"""Places module"""
+from flask import abort, jsonify, request, from flask import abort, jsonify, request, make_response
+
 from api.v1.views import app_views
 from models import storage
 from models.place import Place
 
 
 @app_views.route('/cities/<city_id>/places')
-def all_places(city_id):
-    """Return list of all places in respective to city"""
+def get_places_of_city(city_id):
+    """Retrieves all places of a city"""
     if storage.get("City", city_id) is None:
         abort(404)
-    all_places = storage.all("Place").values()
-    places = [p.to_dict() for p in all_places if p.city_id == city_id]
+    all = storage.all("Place").values()
+    places = [place.to_dict() for place in all if place.city_id == city_id]
     return jsonify(places)
 
 
 @app_views.route('/cities/<city_id>/places', methods=['POST'])
 def add_place(city_id):
-    """Add place based of city_id"""
-    valid_city = storage.get("City", city_id)
-    if not valid_city:
+    """Add place to a city"""
+    city = storage.get("City", city_id)
+    if not city:
         abort(404)
-
-    data = request.get_json(silent=True)
-    if not data:
-        return jsonify({'error': "Not a JSON"}), 400
-    user_id = data.get('user_id', None)
+    body = request.get_json(silent=True)
+    if not body:
+        return make_response(jsonify({'error': "Not a JSON"}), 400)
+    user_id = body.get('user_id', None)
     if not user_id:
-        return jsonify({'error': "Missing user_id"}), 400
-    name = data.get('name', None)
+        return make_response(jsonify({'error': "Missing user_id"}), 400)
+    name = body.get('name', None)
     if not name:
-        return jsonify({'error': "Missing name"}), 400
+        return make_response(jsonify({'error': "Missing name"}), 400)
 
-    data.pop('id', None)
-    data.pop('created_at', None)
-    data.pop('updated_at', None)
-    data.update({'city_id': city_id})
+    body.pop('id', None)
+    body.pop('created_at', None)
+    body.pop('updated_at', None)
+    body.update({'city_id': city_id})
 
     if storage.get("User", user_id) is None:
         abort(404)
     # this place already exists. Just update place with new data
     for place in storage.all("Place").values():
         if place.name == name and place.user_id == user_id:
-            [setattr(place, key, value) for key, value in data.items()]
+            [setattr(place, key, value) for key, value in body.items()]
             place.save()
             return jsonify(place.to_dict()), 200
 
-    place = Place(**data)
+    place = Place(**body)
     place.save()
-    return jsonify(place.to_dict()), 201
+    return make_response(jsonify(place.to_dict()), 201)
 
 
 @app_views.route('/places/<place_id>', methods=['GET', 'PUT', 'DELETE'])
